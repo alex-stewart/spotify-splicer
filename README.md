73116cfd232dbe940777dc7b50b50496eacf5034
alex-stewart <imalexstewart+git@gmail.com>
2020-07-12 19:50:13 +0100

    Boilerplate flask app with temporary /playlists endpoint

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Spotify Splicer
 
 Takes a playlist and a genre and creates a new playlist with the contents spliced. 
+
+## Environment Variables
+
+`SPOTIPY_CLIENT_ID` - Spotify Client ID
+
+`SPOTIPY_CLIENT_SECRET` - Spotify Client Token
+
+`SPOTIPY_REDIRECT_URI` - Application Redirect URL
--- app.py
+++ app.py
@@ -1,19 +1,46 @@
+import os
+from flask import Flask, session, request, redirect
+from flask_session import Session
 import spotipy
-from argparse import ArgumentParser
-from spotipy.oauth2 import SpotifyOAuth
 
-# Parse Input
-parser = ArgumentParser(description='Splice the contents of a playlist with songs from a selected genre.')
-parser.add_argument('input_playlist', type=str, help='A Spotify playlist.')
-parser.add_argument('genre', type=str, help='A Spotify genre.')
-args = parser.parse_args()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
 
-# Create Spotify Client
-scope = "user-library-read"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+Session(app)
 
-# Print Saved Tracks
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path='.cache')
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+
+@app.route('/')
+def index():
+    if request.args.get("code"):
+        session['token_info'] = auth_manager.get_access_token(request.args["code"])
+        return redirect('/')
+
+    if not session.get('token_info'):
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
+           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
+           f'<a href="/playlists">my playlists</a>'
+
+
+@app.route('/sign_out')
+def sign_out():
+    session.clear()
+    return redirect('/')
+
+
+@app.route('/playlists')
+def playlists():
+    if not session.get('token_info'):
+        return redirect('/')
+    else:
+        return spotify.current_user_playlists()
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
